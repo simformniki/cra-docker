dda2079f650397312d4bd9217ccea55e9c40a678
simformniki <niki.p@simformsolutions.com>
2019-07-04 18:25:58 +0530

    make API call to get all dates

--- src/App.js
+++ src/App.js
@@ -2,23 +2,49 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>tag 1</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allDates: []
+    };
+  }
+  componentDidMount() {
+    console.log("component mounted!");
+    this.getDates();
+  }
+  getDates() {
+    return fetch("http://localhost:8080/data")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("HTTP error, status = " + response.status);
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log("Retrieved items:");
+        console.log(json);
+        this.setState({ allDates: [...json] });
+      })
+      .catch(error => {
+        console.log(Error, error);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>All dates</p>
+          <ul>
+            {this.state.allDates.map(item => (
+              <li key={item._id}>{item.date}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
